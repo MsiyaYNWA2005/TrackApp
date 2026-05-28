efaeef65ce04b0b17e6d2f50434b4fd056a84b79
Msiya2841214 <2841214@students.wits.ac.za>
2026-05-29 00:15:05 +0200

    use .env virables for firebase

--- firebaseAdd.js
+++ firebaseAdd.js
@@ -9,7 +9,14 @@ const express= require('express');
 const cors = require('cors');
 
 
-var serviceAccount = require("./schoolTracker.json");
+// var serviceAccount = require("./schoolTracker.json");
+
+var serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+};
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
