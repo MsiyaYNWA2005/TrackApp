02f687f837bc0ec232c35b3950de96ba962f2de3
Msiya2841214 <2841214@students.wits.ac.za>
2026-05-29 11:22:39 +0200

    fixing private key format in railway

--- firebaseAdd.js
+++ firebaseAdd.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 var serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/"/g, ''),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.split('\\n').join('\n'),
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL
 };
 
